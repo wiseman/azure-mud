1841f4c4f5944aea8b89459b606cd8073875d648
John Wiseman <jjwiseman@gmail.com>
2021-10-20 14:17:54 -0700

    Adding EAMs to telex messages.

--- server/src/generators/telex.ts
+++ server/src/generators/telex.ts
@@ -9,77 +9,53 @@ export const actionString = (robot: string) => {
 export const generate = () => {
   var grammar = tracery.createGrammar({
     origin: [
+      '#eam#',
       '#soundEffect#! You watch #visualCues.a# #robot# rolls off the assembly line that #activities#.',
       'You see #visualCues.a# #robot# roll off the assembly line. A part of you wishes you could adopt it for yourself.',
       'You watch as #visualCues.a# #robot# is made.',
       'You watch as #robot.a# that #activities# is made.'
     ],
-    soundEffect: [
-      'Clonk',
-      'Boop',
-      'Cshhhhh',
-      'Fwoop',
-      'Ka-chunk',
-      'Zoop',
-      'Beep beep beep',
-      'Clang',
-      'Clink',
-      'Swoosh'
+    eam: [
+      'SKYKING SKYKING DO NOT ANSWER #codeWord# TIME #time# AUTHENTICATION #nato# #nato#'
     ],
-    robot: [
-      'cute robot',
-      "robot",
-      "machine",
-      "automaton",
-      "android",
-      "synthetic life form"
+    codeWord: [
+      'BILBO',
+      'GANDALF',
+      'NAZGUL',
+      'GOLLUM',
+      'MORDOR'
     ],
-    visualCues: [
-      "eight-legged",
-      "rusted",
-      "weatherbeaten",
-      "shiny chrome",
-      "yellowing plastic",
-      "retro chunky",
-      "vaguely canine",
-      "liquid metal",
-      "small humanoid",
-      "sleek black",
-      "matte gray",
-      "surprisingly glittery",
-      "tread-footed",
-      "multi-armed",
-      "roughly trash-can-shaped",
-      "tall gold",
-      "extremely pale",
-      "sassy",
-      "heavily armored",
-      "helicopter-ish",
-      "cuboid",
-      "red-eyed",
-      "bright green",
-      "dazzlingly fabulous"
+    time: [
+      '22',
+      '34',
+      '19'
     ],
-    activities: [
-      "may not injure a human being or, through inaction, allow a human being to come to harm",
-      "stays alert for Movie Sign",
-      "dreams of electric sheep",
-      "beeps quietly to itself as it tries to resolve a logical paradox",
-      "happily scoots along the floor, sucking up all the dirt in its path",
-      "trundles along slowly, as if depressed",
-      "displays the message '70:Hazard:None'",
-      "emits a sustained high-pitched squeal",
-      "repeats everything you say to it as a question",
-      "hovers unsteadily about two meters off the ground",
-      "beeps and squeals angrily every few minutes",
-      "sings a happy little song when the laundry's done",
-      "occasionally emits a shower of sparks",
-      "falls right to the bottom of the uncanny valley",
-      "...wait this is just a toaster",
-      "is trying to reach you about your car's extended warranty",
-      "seems to be watching a soap opera",
-      "mostly spins around in circles",
-      "constantly refuses to open the pod bay doors"
+    nato: [
+      'ALPHA',
+      'BRAVO',
+      'CHARLIE',
+      'DELTA',
+      'ECHO',
+      'FOXTROT',
+      'GOLF',
+      'HOTEL',
+      'INDIA',
+      'JULIETT',
+      'KILO',
+      'LIMA',
+      'MIKE',
+      'NOVEMBER',
+      'OSCAR',
+      'PAPA',
+      'QUEBEC',
+      'ROMEO',
+      'SIERRA',
+      'TANGO',
+      'UNIFORM',
+      'WHISKEY',
+      'XRAY',
+      'YANKEE',
+      'ZULU'
     ]
   })
 
